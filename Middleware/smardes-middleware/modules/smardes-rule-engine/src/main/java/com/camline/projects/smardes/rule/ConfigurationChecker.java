f06e5027b4aee3985d7debb9f88470c9b6902274
mjh-c <45757182+mjh-c@users.noreply.github.com>
2019-01-31 14:14:03 +0100

    Check for correct configuration before making use of the visitor

--- Middleware/smardes-middleware/modules/smardes-rule-engine/src/main/java/com/camline/projects/smardes/rule/ConfigurationChecker.java
+++ Middleware/smardes-middleware/modules/smardes-rule-engine/src/main/java/com/camline/projects/smardes/rule/ConfigurationChecker.java
@@ -1,16 +1,16 @@
 /*******************************************************************************
  * Copyright (C) 2018-2019 camLine GmbH
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -82,9 +82,9 @@ class ConfigurationChecker extends BaseVisitor<Boolean, RuntimeException> {
 
 	@Override
 	public Boolean visit(Rules rules) {
-		rules.accept(ruleNameVisitor);
 		boolean ok = doVisit(rules.getGlobalVariables(), rules.getPatchMessageBodyPrototypes());
 		if (ok) {
+			rules.accept(ruleNameVisitor);
 			ok = doVisit(rules.getRuleGroup());
 		}
 		return Boolean.valueOf(ok);
