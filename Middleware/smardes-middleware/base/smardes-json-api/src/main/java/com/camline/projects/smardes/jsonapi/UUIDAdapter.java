9a2b8059a7e7ccd8f92ebf4dd8b45cd15bcc37d4
mjh-c <45757182+mjh-c@users.noreply.github.com>
2019-01-31 14:11:17 +0100

    Support also base64 byte array which might be the result of a DB select

--- Middleware/smardes-middleware/base/smardes-json-api/src/main/java/com/camline/projects/smardes/jsonapi/UUIDAdapter.java
+++ Middleware/smardes-middleware/base/smardes-json-api/src/main/java/com/camline/projects/smardes/jsonapi/UUIDAdapter.java
@@ -1,16 +1,16 @@
 /*******************************************************************************
  * Copyright (C) 2018-2019 camLine GmbH
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,6 +21,8 @@
  ******************************************************************************/
 package com.camline.projects.smardes.jsonapi;
 
+import java.nio.ByteBuffer;
+import java.util.Base64;
 import java.util.UUID;
 
 import javax.json.bind.adapter.JsonbAdapter;
@@ -33,6 +35,14 @@ public class UUIDAdapter implements JsonbAdapter<UUID, String> {
 
 	@Override
 	public UUID adaptFromJson(final String obj) throws Exception {
-		return UUID.fromString(obj);
+		try {
+			return UUID.fromString(obj);
+		} catch (@SuppressWarnings("unused") Exception e) {
+			byte[] bytes = Base64.getDecoder().decode(obj);
+			ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+	        long high = byteBuffer.getLong();
+	        long low = byteBuffer.getLong();
+			return new UUID(high, low);
+		}
 	}
 }
