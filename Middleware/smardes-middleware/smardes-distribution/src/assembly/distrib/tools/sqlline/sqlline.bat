1b29c82282a479fe9f61196a441442e7821e987d
mjh-c <45757182+mjh-c@users.noreply.github.com>
2019-02-07 08:34:22 +0100

    Update third-party libraries and components to latest version

--- Middleware/smardes-middleware/smardes-distribution/src/assembly/distrib/tools/sqlline/sqlline.bat
+++ Middleware/smardes-middleware/smardes-distribution/src/assembly/distrib/tools/sqlline/sqlline.bat
@@ -1,7 +1,7 @@
 @echo off
 
 cd /d %~dp0%
-SET JAVA_HOME=..\..\jdk8u192-b12-jre
+SET JAVA_HOME=..\..\${jre.basedir}
 
 :: sqlline.bat - Windows script to launch SQL shell
 "%JAVA_HOME%\bin\java.exe" -cp "%~dp0\lib\*" sqlline.SqlLine %*
